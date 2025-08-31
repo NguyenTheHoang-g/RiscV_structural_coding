ce8d2317858728eccecf5a81f14ab98b50c11c70
NguyenTheHoang-g <hoang.nguyenhoangthe@hcmut.edu.vn>
2025-08-31 22:21:24 +0700

    Update Program_Counter.sv

--- src/Program_Counter.sv
+++ src/Program_Counter.sv
@@ -3,8 +3,8 @@ input logic clk, rst,
 input logic [31:0] PC_in,
 output logic [31:0] PC_out);
 
- always_ff @(posedge clk, negedge rst) begin
- if(~rst)
+ always_ff @(posedge clk, posedge rst) begin
+ if(rst)
  PC_out<= 32'b00;
  else 
  PC_out<= PC_in;
